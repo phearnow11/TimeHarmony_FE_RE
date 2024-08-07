f0f883ba518b782147a3b003e68a7739c5bd6a9a
ThinhPhoenix <laithinh1310@gmail.com>
2024-08-07 08:11:42 +0700

    setup main.js

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('../views/Home.vue').catch(err => {
+        console.error(err);
+      }),
+    },
+]})
+
+export default router;
