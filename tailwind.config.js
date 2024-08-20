8389bb37b666fe06d342e055193ffe882ab04bd0
DavidN2076 <nhancd909@gmail.com>
2024-08-20 11:49:10 +0700

    modify package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
+  ],
+  
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FFBD59',
+        'secondary': '#E6C591',
+        'black-99': '#1E1E1E',
+        'gray-99': '#979b9b',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function({ addBase }) {
+      addBase({
+        'html': {
+          'scrollbar-width': 'none', /* Firefox */
+          '-ms-overflow-style': 'none', /* Internet Explorer 10+ */
+        },
+        'html::-webkit-scrollbar': {
+          'display': 'none', /* Safari and Chrome */
+        },
+      });
+    },
+  ],
+  plugins: [
+]
 }
-
